691658f34808371d1ed0a9ecd4a938a141602818
samukhin <47834890+samukhin@users.noreply.github.com>
2025-10-19 04:50:20 +0300

    Create gray_fast.glsl

---
+++ hyprshade/gray_fast.glsl
@@ -0,0 +1,36 @@
+#version 300 es
+precision highp float;
+
+in vec2 v_texcoord;
+uniform sampler2D tex;
+out vec4 fragColor;
+
+void main() {
+    vec4 pixColor = texture(tex, v_texcoord);
+    vec2 pixelCoord = gl_FragCoord.xy;
+
+    //Выше базовые переменные, ниже логика
+    //vec3 gray = vec3(dot(vec3(0.2126,0.7152,0.0722),pixColor.rgb)); //Оттенок серого через вычисления
+    const float top = 100.0; //Отступ сверху, который нужно сделать серым
+    float average = (pixColor.r + pixColor.g + pixColor.b)/3.0; //Оттенок серого через простое вычисление
+    vec3 gray2 = vec3(average); //Формируем вектор для этого оттенка  vec3(average,average,average)
+
+    // Умножитель скорости. 2.0 означает, что градиент будет в 2 раза короче.
+    const float TRANSITION_MULTIPLIER = 2.0;
+  
+    //Если выше этого значения
+    if (pixelCoord.y <= top) {
+    //То смешиваем пропорционально
+    float mixFactor = ((top - pixelCoord.y) / top) * TRANSITION_MULTIPLIER;
+    // Ограничиваем значение, чтобы оно не превышало 1.0
+    mixFactor = clamp(mixFactor, 0.0, 1.0);
+
+    fragColor = vec4(
+    vec3(mix(pixColor.rgb,gray2,mixFactor)),
+    pixColor.a);
+    } else {
+    //Если нет, остаёмся со старым цветом
+    fragColor = pixColor;
+    }
+  
+}
